c42c4f3615ea431f2b0d6ab8fff4dccbef0b9e6c
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-06-18 22:33:23 -0500

    Added data persistence with LocalStorage

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,26 @@
 import React, {useState} from 'react';
-
-import '../styles/App.css';
 import { AppUI } from './AppUI';
+import '../styles/App.css';
 
 
-const defaultTodos = [
+/*const defaultTodos = [
 	{ text: 'Study react', completed: true },
 	{ text: 'Sleep', completed: true },
 	{ text: 'Read a book', completed: false },
-];
+];*/
 
 function App() {
+	const localStorageTodos = localStorage.getItem('TODOS_V1');
+	let parsedTodos;
 
-	const [todos, setTodos] = useState(defaultTodos);
+	if(!localStorageTodos){
+		localStorage.setItem('TODOS_V1', JSON.stringify([]));
+		parsedTodos = [];
+	} else {
+		parsedTodos = JSON.parse(localStorageTodos);
+	}
+
+	const [todos, setTodos] = useState(parsedTodos);
 	const [searchValue, setSearchValue] = useState('');
 
 	const completedTodos = todos.filter((todo) => !!todo.completed).length;
@@ -29,11 +37,18 @@ function App() {
 		});
 	}
 
+	const saveTodos = (newTodos) => {
+		const stringifiedTodos = JSON.stringify(newTodos);
+		localStorage.setItem('TODOS_V1', stringifiedTodos);
+		setTodos(newTodos);
+	}
+
 	const completeTodo = (text) => {
 		const todoIndex = todos.findIndex(todo => todo.text == text);	
 		const newTodos = [...todos];
 		newTodos[todoIndex].completed = true;
 		setTodos(newTodos);
+		saveTodos(newTodos);
 		/*todos[todoIndex] = {
 			text: todos[todoIndex].text,
 			completed: true,
